bba62d8e0cf43014df33231c5d6703943a409742
khzs <khzsolt@gmail.com>
2025-05-28 12:46:01 +0200

    v1

---
+++ one-time-setup-Linux.sh
@@ -0,0 +1,42 @@
+#
+echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
+
+#
+sudoer()
+{
+  sudo apt install bat micro l3afpad libicu-dev -y
+  # hstr
+  # sudo snap install yazi --classic
+}
+
+
+hostile()
+{
+  # hstr
+  echo "https://github.com/dvorka/hstr/releases"
+
+  # bat
+
+  # micro
+  mkdir -p ~/.local/bin
+  cd ~/.local/bin
+  curl https://getmic.ro | bash
+  cd -
+}
+
+
+if sudo -l &>/dev/null; then
+  sudoer
+else
+  hostile
+fi
+
+
+
+# sourcing
+sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
+sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
+echo "source ~/.config/dotfiles-core/.bashrc" >> ~/.bashrc
+
+#
+source ~/.config/dotfiles-core/one-time-setup.sh
