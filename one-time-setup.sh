0996c10cc622c76245c08a2abbebcc7bc6bcddca
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-16 19:07:26 +0200

    move into subfolder for lndir

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -3,7 +3,7 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 
 
 # symlink
-lndir ~/.config/dotfiles-core ~/.config
+lndir ~/.config/dotfiles-core/config-overlay ~/.config
 
 # edit
 mkdir -p ~/.local/bin
