97f0e1e8f22f9323cc98ee13e773c82f18400dbb
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-17 13:28:21 +0200

    newlines

--- one-time-setup-Linux.sh
+++ one-time-setup-Linux.sh
@@ -13,11 +13,11 @@ chsh -s $(which zsh)
 #
 sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
 sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
-echo "source ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
+echo "\nsource ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
 
 # eloszor fel kell loni az ujonnan installalt zsh-t, hogy populalja default configgal, es csak utana beleverni
 # echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}" -> .zshrc eleje
-# echo "source ~/.config/dotfiles-core/.linuxzshrc" >> ~/.zshrc
+# echo "\nsource ~/.config/dotfiles-core/.linuxzshrc" >> ~/.zshrc
 
 #
 source ~/.config/dotfiles-core/one-time-setup.sh
