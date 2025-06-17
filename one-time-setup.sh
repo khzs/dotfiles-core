7e658008dabfccf20c9bcb50a936ff4b5effcac3
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-17 13:14:13 +0200

    remove ms edit and hstr

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -4,8 +4,3 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 
 # symlink
 lndir ~/.config/dotfiles-core/config-overlay ~/.config
-
-# edit
-mkdir -p ~/.local/bin
-wget -qO- https://github.com/microsoft/edit/releases/download/v1.0.0/edit-1.0.0-x86_64-linux-gnu.xz | unxz > ~/.local/bin/edit
-chmod +x ~/.local/bin/edit
