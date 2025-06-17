592ac30531af8bf914f1abd9338941c39e9835be
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-17 13:15:47 +0200

    remove non-sudo

--- one-time-setup-Linux.sh
+++ one-time-setup-Linux.sh
@@ -2,37 +2,13 @@
 echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 
 #
-sudoer()
-{
-  # xutils-dev    : for lndir
-  sudo apt install -y bat micro l3afpad \
-    xutils-dev
+sudo apt install -y bat micro l3afpad \
+  xutils-dev
+# xutils-dev    : for lndir
 
-  sudo snap install yazi --classic
-}
+sudo snap install yazi --classic
 
-
-hostile()
-{
-  # bat
-
-  # micro
-  mkdir -p ~/.local/bin
-  cd ~/.local/bin
-  curl https://getmic.ro | bash
-  cd -
-}
-
-
-if sudo -l &>/dev/null; then
-  sudoer
-else
-  hostile
-fi
-
-
-
-# sourcing
+#
 sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
 sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
 echo "source ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
