eacd7c530d9c8566c2d62d361380736793c4fadb
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-17 11:16:15 +0200

    new naming scheme

--- one-time-setup-Linux.sh
+++ one-time-setup-Linux.sh
@@ -42,7 +42,7 @@ fi
 # sourcing
 sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
 sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
-echo "source ~/.config/dotfiles-core/.bashrc" >> ~/.bashrc
+echo "source ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
 
 #
 source ~/.config/dotfiles-core/one-time-setup.sh
