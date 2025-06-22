cef32964ce3ff2c4772a55cccb4c5788324e0e49
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-06-22 23:13:21 +0200

    hard copy .zshrc

--- one-time-setup-Linux.sh
+++ one-time-setup-Linux.sh
@@ -5,19 +5,16 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 sudo apt install -y bat micro l3afpad petname zsh zsh-autosuggestions \
   xutils-dev
 # xutils-dev    : for lndir
-
 sudo snap install yazi --classic
 
+#
 chsh -s $(which zsh)
+cp ~/.config/dotfiles-core/copy/.zshrc ~/
 
 #
 sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
 sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
 echo "\nsource ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
-
-# eloszor fel kell loni az ujonnan installalt zsh-t, hogy populalja default configgal, es csak utana beleverni
-# echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}" -> .zshrc eleje
-# echo "\nsource ~/.config/dotfiles-core/.linuxzshrc" >> ~/.zshrc
 
 #
 source ~/.config/dotfiles-core/one-time-setup.sh
