777d2d7f36a9fb542eea066041cce5e4d50b6214
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2025-07-14 20:14:15 +0200

    git config --global ...

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,6 +1,10 @@
 #
 echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 
+#
+git config --global core.editor "micro"
+git config --global pull.rebase true
+git config --global push.autoSetupRemote true
 
 # symlink
 lndir ~/.config/dotfiles-core/config-overlay ~/.config
