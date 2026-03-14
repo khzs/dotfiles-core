b5e38bba571f50791aaa4b0fc9623b62df686f5f
khzs <khzsolt@gmail.com>
2026-03-14 14:56:45 +0100

    fix: echo -e

--- one-time-setup-Linux.sh
+++ one-time-setup-Linux.sh
@@ -4,7 +4,7 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 #
 sed -i '/#umask 022/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.profile
 sed -i '/# for examples/a\\necho "entering $(realpath "${BASH_SOURCE[0]}")"' ~/.bashrc
-echo "\nsource ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
+echo -e "\nsource ~/.config/dotfiles-core/.linuxbashrc" >> ~/.bashrc
 
 #
 sudo apt install -y zsh-autosuggestions xutils-dev                 # xutils-dev : for lndir
