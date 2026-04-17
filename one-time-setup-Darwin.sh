3b9a6dbe5d17826087cdf5ee61ef1d5402279fdd
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-17 14:05:48 +0200

    zed

--- one-time-setup-Darwin.sh
+++ one-time-setup-Darwin.sh
@@ -4,6 +4,7 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 #
 brew install zsh-autosuggestions fzf zoxide
 ln -s "/Applications/Beyond Compare.app/Contents/MacOS/bcomp" ~/.local/bin/bcompare
+ln -s "/Applications/Zed.app/Contents/MacOS/cli" ~/.local/bin/zed
 
 #
 # echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}" -> .zshrc eleje
