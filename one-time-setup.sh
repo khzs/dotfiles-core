d79bb5f2b78346892176132db13e175d9e97b66c
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-29 22:33:36 +0200

    gitignore global

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -5,11 +5,13 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 git config --global core.editor "micro"
 git config --global pull.rebase true
 git config --global push.autoSetupRemote true
+git config --global core.excludesFile ~/.gitignore_global
 
 # symlink
 lndir ~/.config/dotfiles-core/config-overlay ~/.config
 mkdir -p ~/.config/fresh && touch ~/.config/fresh/config.json
 mkdir -p ~/.config/ghostty && touch ~/.config/ghostty/config.ghostty
+touch ~/.gitignore_global
 
 #
 brew install --cask font-adwaita-mono-nerd-font      # Mono = basic; Mono ... Mono = smaller icons in yazi
