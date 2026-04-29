e160d92d3f97736f6c3d3ed202c96e70556fbde3
Zsolt Kocsi-Horvath <khzsolt@gmail.com>
2026-04-29 22:33:36 +0200

    gitignore global

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -5,9 +5,11 @@ echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}"
 git config --global core.editor "micro"
 git config --global pull.rebase true
 git config --global push.autoSetupRemote true
+git config --global core.excludesFile ~/.gitignore_global
 
 # symlink
 lndir ~/.config/dotfiles-core/config-overlay ~/.config
+[ ! -f ~/.gitignore_global ] && cp ~/.config/dotfiles-core/copy/.gitignore_global.base ~/.gitignore_global
 
 #
 uname -r | grep WSL   || brew install --cask font-adwaita-mono-nerd-font      # Mono = basic; Mono ... Mono = smaller icons in yazi
