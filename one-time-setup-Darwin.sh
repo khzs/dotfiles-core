3240aed964c80bceb69fd9df9a23ac20364f22fd
khzs <khzsolt@gmail.com>
2026-05-21 16:14:48 +0200

    todo: fix this

--- one-time-setup-Darwin.sh
+++ one-time-setup-Darwin.sh
@@ -6,7 +6,7 @@ brew install zsh-autosuggestions fzf zoxide
 
 #
 # echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}" -> .zshrc eleje
-echo "\nsource ~/.config/dotfiles-core/.darwinzshrc" >> ~/.zshrc
+# echo "\nsource ~/.config/dotfiles-core/.darwinzshrc" >> ~/.zshrc
 
 #
 source ~/.config/dotfiles-core/one-time-setup.sh
