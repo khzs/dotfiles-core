4a5d275efb01292176546bc303fce75aad7d757d
khzs <khzsolt@gmail.com>
2026-07-01 17:19:54 +0200

    mac keyswap import

--- one-time-setup-Darwin.sh
+++ one-time-setup-Darwin.sh
@@ -6,6 +6,11 @@ brew install zsh-autosuggestions wget yt-dlp ffmpeg fzf zoxide
 ln -s "/Applications/Beyond Compare.app/Contents/MacOS/bcomp" ~/.local/bin/bcompare
 ln -s "/Applications/Zed.app/Contents/MacOS/cli" ~/.local/bin/zed
 
+if [ ! -f ~/Library/LaunchAgents/com.user.keyswap.plist ]; then
+  curl -fsSL https://raw.githubusercontent.com/khzs/keyboard-everything/main/com.user.keyswap.plist -o ~/Library/LaunchAgents/com.user.keyswap.plist
+  launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/com.user.keyswap.plist
+fi
+
 #
 # echo "entering ${BASH_SOURCE[0]:-${(%):-%N}}" -> .zshrc eleje
 echo "\nsource ~/.config/dotfiles-core/.darwinzshrc" >> ~/.zshrc
